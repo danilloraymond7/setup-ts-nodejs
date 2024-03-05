ca5ff920ff40011384765fb0b9ac2c23342bddeb
Danillo Raymund <33788284+danilloraymond7@users.noreply.github.com>
2024-03-05 11:28:04 -0300

    style(eslit): padronização de estilo de codigo
    evitar ficar ajustando formatação de codigo

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,6 @@
-console.log("hello world from serverr.ts")
-console.log("hello world 2")
-console.log("hello world 3")
+console.log('hello world from serverr.ts')
+console.log('hello world 2')
+console.log('hello world 3')
+console.log('dsds')
+
+const a = 1
